a9dedbfa63761400c3dd13baef09d7cf3749f2f3
Kris Hokr <khokr@ucdavis.edu>
2022-04-06 12:27:59 -0700

    updated progress bar

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,7 +1,113 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import './Styles/ProgressBar.css';
 
-export default function ProgressBar() {
+export default function ProgressBar(props) {
+  
+
+
+  // const barStyle =  {
+  //   height: 5,
+  //   width: '100%',
+  //   borderRadius: 50
+  // }
+
+
+  // const circleStyle = {
+  //   borderRadius: '100%',
+  //   height: '50px',
+  //   width: '50px'
+  // }
+
+  // const containerStyle = {
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   alignItems: 'center'
+  // }
+  
+  // const barContainerStyle = {
+  //     display: 'flex',
+  //     justifyContent: 'center',
+  //     width: '10%',
+  //     height: 50
+  // }
+
+
+  // const fontStyle = {
+  //   fontSize: '80%',
+  //   fontWeight: 500
+  // }
+
+  // const circleContainerStyle = {
+  //     display: 'flex',
+  //     flexDirection: 'column',
+  //     alignItems: 'center',
+  //     justifyContent: 'center',
+  //     width: '11%'
+  // }
+
   return (
-    <div>ProgressBar</div>
+    <div className='container'>
+
+      <div className='column-container' >
+        <div className='row-container'>
+          <div className='circle' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+          <div className='bar' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+        </div>
+        
+        <h3>Step 1</h3>
+      </div>
+
+      <div className='column-container' >
+        <div className='row-container'>
+          <div className='circle' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+          <div className='bar' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+        </div>
+        
+        <h3>Step 2</h3>
+      </div>
+      <div className='column-container' >
+        <div className='row-container'>
+          <div className='circle' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+          <div className='bar' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+        </div>
+        
+        <h3>Step 3</h3>
+      </div>
+      
+
+      <div className='final-circle-container'>
+        <div className='circle' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+        <h3>Step 4</h3>
+      </div>
+     
+
+      {/* <div className='bar-container'>
+        <div className='bar' style={{backgroundColor: (props.step >= 1) ? '#022851' : '#e0e0de'}}></div>
+      </div> */}
+
+      {/* <div className='circle-container'>
+        <div className='circle' style={{backgroundColor: (props.step >= 2) ? '#022851' : '#e0e0de'}}></div>
+        <h3>Step 2</h3>
+      </div>
+
+      <div className='bar-container'>
+        <div className='bar' style={{backgroundColor: (props.step >= 2) ? '#022851' : '#e0e0de'}}></div>
+      </div>
+
+      <div className='circle-container'>
+        <div className='circle' style={{backgroundColor: (props.step >= 3) ? '#022851' : '#e0e0de'}}></div>
+        <h3>Step 3</h3>
+      </div>
+      
+      <div className='bar-container'>
+        <div className='bar' style={{backgroundColor: (props.step >= 3) ? '#022851' : '#e0e0de'}}></div>
+      </div>
+
+      <div className='circle-container'>
+        <div className='circle' style={{backgroundColor: (props.step === 4) ? '#022851' : '#e0e0de'}}></div>
+        <h3>Step 4</h3>
+      </div> */}
+
+  </div>
   )
 }
