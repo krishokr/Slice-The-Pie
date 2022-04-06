fd81fb9f82c8fc846d016980ccfd769db75bddce
Kris Hokr <kristoferhokr@gmail.com>
2022-04-06 15:06:25 -0700

    styled splash page

--- src/Splash.js
+++ src/Splash.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Styles/Splash.css';
 
 export default function Splash(props) {
   return (
     <div className='splash-container'>
         <h1>Welcome to Slice the Pie!</h1>
-        <h3>You are now a provost of a major university. It is your job to not only worry about where your revenue comes from, but also to decide how to allocate those funds to which sectors.</h3>
+        <h3>You are now a provost of a major university. <br></br><br></br>It is your job to not only worry about where your revenue comes from, but also to decide how to allocate those funds to which sectors.</h3>
         <h3>Can you guess where your funds come from? <br/> How about where those funds go?</h3>
-        <h4>How much will you give to research? Teaching? How about student services?</h4>
-        <h5>Sounds stressful?</h5>
+        <h4>How much will you give to research?<br></br> <br></br>How about student services? <br></br><br></br>Teaching?</h4>
+        <h3 className='button-label'>Sound stressful?</h3>
         <button onClick={props.changeState}>Nah, let's do this!</button>
     </div>
   )
